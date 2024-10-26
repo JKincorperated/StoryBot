1584e8f3fbbe7fd2650066eb606981162947eeab
JKincorperated <73645805+JKincorperated@users.noreply.github.com>
2024-10-26 13:32:52 +0100

    Update

--- start.sh
+++ start.sh
@@ -2,15 +2,15 @@
 
 HASH=$(git rev-parse HEAD)
 git fetch --all
-LOCAL_HASH=$(cat .git/refs/heads/main)
-echo "Current commit hash: $HASH"
-echo "Local commit hash: $LOCAL_HASH"
+NEWEST_HASH=$(git rev-parse `git branch -r --sort=committerdate | tail -1`)
+echo "Current commit hash: $NEWEST_HASH"
+echo "Local commit hash: $HASH"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-if [ "$HASH" != "$LOCAL_HASH" ]; then
+if [ "$HASH" != "$NEWEST_HASH" ]; then
     echo "Updating from github"
     git pull
     echo "Installing npm packages"
